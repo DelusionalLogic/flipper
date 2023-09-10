0a90f502543b3288c1f0430750a35297555f3cef
Jesper Jensen <jesper@jnsn.dev>
2023-09-10 16:11:35 +0200

    Remove unused  noise functions

--- main.c
+++ main.c
@@ -12,15 +12,15 @@
 #include <unistd.h>
 #include <math.h>
 
-static const uint8_t dither[]  = {
-  0x03, 0x83, 0x23, 0xa3, 0x0b, 0x8b, 0x2b, 0xab, 
-  0xc3, 0x43, 0xe3, 0x63, 0xcb, 0x4b, 0xeb, 0x6b, 
-  0x33, 0xb3, 0x13, 0x93, 0x3b, 0xbb, 0x1b, 0x9b, 
-  0xf3, 0x73, 0xd3, 0x53, 0xfb, 0x7b, 0xdb, 0x5b, 
-  0x0f, 0x8f, 0x2f, 0xaf, 0x07, 0x87, 0x27, 0xa7, 
-  0xcf, 0x4f, 0xef, 0x6f, 0xc7, 0x47, 0xe7, 0x67, 
-  0x3f, 0xbf, 0x1f, 0x9f, 0x37, 0xb7, 0x17, 0x97, 
-  0xff, 0x7f, 0xdf, 0x5f, 0xf7, 0x77, 0xd7, 0x57
+static const uint8_t dither[] = {
+	0x03, 0x83, 0x23, 0xa3, 0x0b, 0x8b, 0x2b, 0xab,
+	0xc3, 0x43, 0xe3, 0x63, 0xcb, 0x4b, 0xeb, 0x6b,
+	0x33, 0xb3, 0x13, 0x93, 0x3b, 0xbb, 0x1b, 0x9b,
+	0xf3, 0x73, 0xd3, 0x53, 0xfb, 0x7b, 0xdb, 0x5b,
+	0x0f, 0x8f, 0x2f, 0xaf, 0x07, 0x87, 0x27, 0xa7,
+	0xcf, 0x4f, 0xef, 0x6f, 0xc7, 0x47, 0xe7, 0x67,
+	0x3f, 0xbf, 0x1f, 0x9f, 0x37, 0xb7, 0x17, 0x97,
+	0xff, 0x7f, 0xdf, 0x5f, 0xf7, 0x77, 0xd7, 0x57
 };
 
 static void plot(struct RenderContext *ctx, uint16_t x, uint16_t y, uint8_t v) {
@@ -96,6 +96,10 @@ float lerpf(float a, float b, float t) {
 	return a * (1-t) + b * (t);
 }
 
+float slerpf(float min, float max, float v) {
+	return lerpf(min, max, v * v * (3-2*v));
+}
+
 float sample(const uint8_t *tex, float x, float y) {
 	uint16_t ix = x, iy = y;
 	float fx = x - ix, fy = y - iy;
@@ -114,33 +118,6 @@ float sample(const uint8_t *tex, float x, float y) {
 	return lerpf(a, b, fy)/255;
 }
 
-static int SEED = 0;
-static int hasht[] = {
-    208, 34,  231, 213, 32,  248, 233, 56,  161, 78,  24,  140, 71,  48,  140,
-    254, 245, 255, 247, 247, 40,  185, 248, 251, 245, 28,  124, 204, 204, 76,
-    36,  1,   107, 28,  234, 163, 202, 224, 245, 128, 167, 204, 9,   92,  217,
-    54,  239, 174, 173, 102, 193, 189, 190, 121, 100, 108, 167, 44,  43,  77,
-    180, 204, 8,   81,  70,  223, 11,  38,  24,  254, 210, 210, 177, 32,  81,
-    195, 243, 125, 8,   169, 112, 32,  97,  53,  195, 13,  203, 9,   47,  104,
-    125, 117, 114, 124, 165, 203, 181, 235, 193, 206, 70,  180, 174, 0,   167,
-    181, 41,  164, 30,  116, 127, 198, 245, 146, 87,  224, 149, 206, 57,  4,
-    192, 210, 65,  210, 129, 240, 178, 105, 228, 108, 245, 148, 140, 40,  35,
-    195, 38,  58,  65,  207, 215, 253, 65,  85,  208, 76,  62,  3,   237, 55,
-    89,  232, 50,  217, 64,  244, 157, 199, 121, 252, 90,  17,  212, 203, 149,
-    152, 140, 187, 234, 177, 73,  174, 193, 100, 192, 143, 97,  53,  145, 135,
-    19,  103, 13,  90,  135, 151, 199, 91,  239, 247, 33,  39,  145, 101, 120,
-    99,  3,   186, 86,  99,  41,  237, 203, 111, 79,  220, 135, 158, 42,  30,
-    154, 120, 67,  87,  167, 135, 176, 183, 191, 253, 115, 184, 21,  233, 58,
-    129, 233, 142, 39,  128, 211, 118, 137, 139, 255, 114, 20,  218, 113, 154,
-    27,  127, 246, 250, 1,   8,   198, 250, 209, 92,  222, 173, 21,  88,  102,
-    219
-};
-
-int noise2(int x, int y) {
-    int tmp = hasht[(y + SEED) % 256];
-    return hasht[(tmp + x) % 256];
-}
-
 float hash(float p) {
 	float f;
 	p = modff(p * 0.011, &f);
@@ -155,52 +132,6 @@ float noise(float x) {
 	float f = modff(x, &i);
 	float u = f * f * (3.0 - 2.0 * f);
 	return lerpf(hash(i), hash(i + 1.0), u);
-}
-
-// 2D Random
-float random2d(float x, float y) {
-	float dot = 12.9898 * x + 78.233 * y;
-	float unused;
-	return modff(dot, &unused) * 43758.5453123;
-}
-
-float smoothstep(float min, float max, float v) {
-    return lerpf(min, max, v * v * (3-2*v));
-}
-
-float noise2d(float x, float y) {
-    int x_int = x;
-    int y_int = y;
-    float x_frac = x - x_int;
-    float y_frac = y - y_int;
-
-    int s = noise2(x_int, y_int);
-    int t = noise2(x_int+1, y_int);
-    int u = noise2(x_int, y_int+1);
-    int v = noise2(x_int+1, y_int+1);
-
-    float low = smoothstep(s, t, x_frac);
-    float high = smoothstep(u, v, x_frac);
-    return smoothstep(low, high, y_frac);
-}
-
-float perlin2d(float x, float y, float freq, int depth) {
-    float xa = x*freq;
-    float ya = y*freq;
-    float amp = 1.0;
-    float fin = 0;
-    float div = 0.0;
-
-    int i;
-    for(i=0; i<depth; i++) {
-        div += 256 * amp;
-        fin += noise2d(xa, ya) * amp;
-        amp /= 2;
-        xa *= 2;
-        ya *= 2;
-    }
-
-    return fin/div;
 }
 
 static bool process(struct RenderContext *ctx) {
